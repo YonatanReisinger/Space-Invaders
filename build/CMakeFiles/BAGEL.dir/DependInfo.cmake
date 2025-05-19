c47c66d096866af30e1ac31ab2d4143064d4c8ca
yonatan.r@razor-labs.com <yonatan.r@razor-labs.com>
2025-05-19 19:14:26 +0300

    Added new code from lecture 8

---
+++ build/CMakeFiles/BAGEL.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yonatan.r/CLionProjects/BAGEL/main.cpp" "CMakeFiles/BAGEL.dir/main.cpp.o" "gcc" "CMakeFiles/BAGEL.dir/main.cpp.o.d"
+  "/home/yonatan.r/CLionProjects/BAGEL/tests.cpp" "CMakeFiles/BAGEL.dir/tests.cpp.o" "gcc" "CMakeFiles/BAGEL.dir/tests.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yonatan.r/CLionProjects/BAGEL/build/lib/SDL/CMakeFiles/SDL3-static.dir/DependInfo.cmake"
+  "/home/yonatan.r/CLionProjects/BAGEL/build/lib/SDL_image/CMakeFiles/SDL3_image-static.dir/DependInfo.cmake"
+  "/home/yonatan.r/CLionProjects/BAGEL/build/lib/box2d/src/CMakeFiles/box2d.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
